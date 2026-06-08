ba5ad7fa601fdcc2309c82d44227e853c811c4c7
Tarun <tarun@gmail.com>
2026-06-08 22:28:52 +0530

    Added menu operation handling

--- src/main.c
+++ src/main.c
@@ -9,6 +9,31 @@ int main()
         printf("\n1.Draw 2.Delete 3.Modify 4.Display 5.Exit\n");
         printf("Choice: ");
         scanf("%d", &choice);
+        switch(choice)
+{
+    case 1:
+        printf("Draw operation selected\n");
+        break;
+
+    case 2:
+        printf("Delete operation selected\n");
+        break;
+
+    case 3:
+        printf("Modify operation selected\n");
+        break;
+
+    case 4:
+        printf("Display operation selected\n");
+        break;
+
+    case 5:
+        printf("Exiting...\n");
+        break;
+
+    default:
+        printf("Invalid choice\n");
+}
 
     } while(choice != 5);
 
