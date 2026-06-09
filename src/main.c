e2aea75ecd4fd7cac85ec69405b413833262e9da
Tarun <tarun@gmail.com>
2026-06-09 23:55:37 +0530

    Updated all menu operation modules

--- src/main.c
+++ src/main.c
@@ -12,21 +12,35 @@ printf("1.Draw 2.Delete 3.Modify 4.Display 5.Exit\n");
         scanf("%d", &choice);
         switch(choice)
 {
-    case 1:
-        printf("Draw operation selected\n");
-        break;
+case 1:
+{
+    printf("Draw module selected\n");
+    printf("Here user can draw Line, Rectangle, or Circle\n");
+    break;
+}
 
-    case 2:
-        printf("Delete operation selected\n");
-        break;
+case 2:
+{
+    printf("Delete module selected\n");
+    printf("Here user can delete an existing shape\n");
+    break;
+}
 
-    case 3:
-        printf("Modify operation selected\n");
-        break;
+case 3:
+{
+    printf("Modify module selected\n");
+    printf("Here user can update shape coordinates\n");
+    break;
+}
 
-    case 4:
-        printf("Display operation selected\n");
-        break;
+case 4:
+{
+    printf("\n===== DISPLAY MENU =====\n");
+    printf("Shapes stored in canvas:\n");
+    printf("No shapes available currently.\n");
+    printf("Add shapes using Draw option.\n");
+    break;
+}
 
     case 5:
         printf("Exiting...\n");
